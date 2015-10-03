2a96780a84d96db6333a68be39472b8247e1ea54
TAGOMORI Satoshi <tagomoris@gmail.com>
2015-10-03 21:40:59 +0900

    make webapp tree

---
+++ webapp/ruby/app.rb
@@ -0,0 +1,110 @@
+require 'sinatra/base'
+require 'mysql2-cs-bind'
+require 'tilt/erubis'
+require 'erubis'
+
+module Isucon5
+  module TimeWithoutZone
+    def to_s
+      strftime("%F %H:%M:%S")
+    end
+  end
+  ::Time.prepend TimeWithoutZone
+end
+
+class Isucon5::WebApp < Sinatra::Base
+  use Rack::Session::Cookie
+  set :erb, escape_html: true
+  set :public_folder, File.expand_path('../../static', __FILE__)
+  set :session_secret, ENV['ISUCON5_SESSION_SECRET'] || 'tonymoris'
+  set :protection, true
+
+  helpers do
+    def config
+      @config ||= {
+        db: {
+          host: ENV['ISUCON5_DB_HOST'] || 'localhost',
+          port: ENV['ISUCON5_DB_PORT'] && ENV['ISUCON5_DB_PORT'].to_i,
+          username: ENV['ISUCON5_DB_USER'] || 'root',
+          password: ENV['ISUCON5_DB_PASSWORD'],
+          database: ENV['ISUCON5_DB_NAME'] || 'isucon5q',
+        },
+      }
+    end
+
+    def db
+      return Thread.current[:isucon5_db] if Thread.current[:isucon5_db]
+      client = Mysql2::Client.new(
+        host: config[:db][:host],
+        port: config[:db][:port],
+        username: config[:db][:username],
+        password: config[:db][:password],
+        database: config[:db][:database],
+        reconnect: true,
+      )
+      client.query_options.merge!(symbolize_keys: true)
+      Thread.current[:isucon5_db] = client
+      client
+    end
+
+    def authenticate(email, password)
+      query = <<SQL
+SELECT u.id AS id, u.account_name AS account_name, u.nick_name AS nick_name, u.email AS email
+FROM users u
+JOIN salts s ON u.id = s.user_id
+WHERE u.email = ? AND u.passhash = SHA2(CONCAT(?, s.salt), 512)
+SQL
+      result = db.xquery(query, email, password).first
+      unless result
+        raise Isucon5::AuthenticationError
+      end
+      session[:user_id] = result[:id]
+      result
+    end
+
+    def current_user
+      return @user if @user
+      unless session[:user_id]
+        return nil
+      end
+      @user = db.xquery('SELECT id, account_name, nick_name, email FROM users WHERE id=?', session[:user_id]).first
+      unless @user
+        session[:user_id] = nil
+        session.clear
+        raise Isucon5::AuthenticationError
+      end
+      @user
+    end
+
+    def authenticated!
+      unless current_user
+        redirect '/login'
+      end
+    end
+  end
+
+  get '/login' do
+    session.clear
+    # TBD
+    # erb :login, layout: false, locals: { message: '高負荷に耐えられるSNSコミュニティサイトへようこそ!' }
+  end
+
+  post '/login' do
+    authenticate params['email'], params['password']
+    redirect '/'
+  end
+
+  get '/logout' do
+    session.clear
+    redirect '/login'
+  end
+
+  get '/' do
+    authenticated!
+    # TBD
+  end
+
+  get '/initialize' do
+    # TODO any proc?
+  end
+end
