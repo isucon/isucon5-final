72b445b55f4a07b9d213a1230f2ce4f645344af8
Ryuta Kamizono <kamipo@gmail.com>
2015-10-29 21:18:42 +0900

    Revert "Add GOROOT"
    This reverts commit 45881a38feb2382178eeb03bf211f501d7ce7884.

--- provisioning/api/files/env.sh
+++ provisioning/api/files/env.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export PATH=/usr/local/bin:/home/isucon/.local/ruby/bin:/home/isucon/.local/node/bin:/home/isucon/.local/python3/bin:/home/isucon/.local/perl/bin:/home/isucon/.local/php/bin:/home/isucon/.local/php/sbin:/home/isucon/.local/go/bin:/home/isucon/.local/scala/bin:$PATH
-export GOROOT=/home/isucon/.local/go
+export GOPATH=/home/isucon/gocode
 
 exec $*
