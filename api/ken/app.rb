3c07a634ffa818a733c4b7bbd019102f8729ba27
TAGOMORI Satoshi <tagomoris@gmail.com>
2015-10-29 16:56:30 +0900

    fix to specify csv file encoding explicitly

--- api/ken/app.rb
+++ api/ken/app.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 require 'csv'
 
 csvfile = File.expand_path("../x-ken-all.utf8.csv", __FILE__)
@@ -6,7 +7,7 @@ data = {} # num => [ data1, data2, ... ]
 # data is [ "北海道 札幌市 中央区旭ケ丘" ]
 
 # 13110,"153  ","1530042","ﾄｳｷｮｳﾄ","ﾒｸﾞﾛｸ","ｱｵﾊﾞﾀﾞｲ","東京都","目黒区","青葉台",0,0,1,0,0,0
-CSV.foreach(csvfile) do |row|
+CSV.foreach(csvfile, "r:utf-8") do |row|
   zipcode = row[2]
   address = "#{row[6]} #{row[7]} #{row[8]}"
   data[zipcode] ||= []
