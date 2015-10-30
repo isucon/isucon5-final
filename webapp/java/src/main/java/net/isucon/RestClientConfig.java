741ddac3107641baf272508c42abb5692c276bff
Toshiaki Maki <makingx@gmail.com>
2015-10-31 05:02:46 +0900

    Support Apache HTTP Client

--- webapp/java/src/main/java/net/isucon/RestClientConfig.java
+++ webapp/java/src/main/java/net/isucon/RestClientConfig.java
@@ -1,16 +1,20 @@
 package net.isucon;
 
-import java.io.IOException;
-import java.net.HttpURLConnection;
 import java.security.SecureRandom;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 
-import javax.net.ssl.*;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.X509TrustManager;
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -40,18 +44,9 @@ public class RestClientConfig {
 
         SSLSocketFactory socketFactory = sslContext.getSocketFactory();
         HostnameVerifier verifier = (h, s) -> true;
-        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory() {
-            @Override
-            protected void prepareConnection(HttpURLConnection connection,
-                    String httpMethod) throws IOException {
-                if (connection instanceof HttpsURLConnection) {
-                    HttpsURLConnection con = (HttpsURLConnection) connection;
-                    con.setSSLSocketFactory(socketFactory);
-                    con.setHostnameVerifier(verifier);
-                }
-                super.prepareConnection(connection, httpMethod);
-            }
-        };
-        return new RestTemplate(factory);
+        HttpClient client = HttpClientBuilder.create().setSSLSocketFactory(
+                new SSLConnectionSocketFactory(socketFactory, verifier))
+                .build();
+        return new RestTemplate(new HttpComponentsClientHttpRequestFactory((client)));
     }
 }
