6f556c4145f07fd9c3de4ab04ee1ea025de947c1
Toshiaki Maki <makingx@gmail.com>
2015-10-31 03:58:19 +0900

    Add _JAVA_OPTIONS

--- provisioning/image/files/env.sh
+++ provisioning/image/files/env.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 export PATH=/usr/local/bin:/home/isucon/.local/ruby/bin:/home/isucon/.local/node/bin:/home/isucon/.local/python3/bin:/home/isucon/.local/perl/bin:/home/isucon/.local/php/bin:/home/isucon/.local/php/sbin:/home/isucon/.local/go/bin:/home/isucon/.local/scala/bin:$PATH
 export GOPATH=/home/isucon/gocode
+export _JAVA_OPTIONS="-Dfile.encoding=UTF-8"
 
 exec $*
