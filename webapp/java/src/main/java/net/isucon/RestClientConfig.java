862c60ca943386cf589c142f4083ac699678777c
Toshiaki Maki <makingx@gmail.com>
2015-10-31 04:46:16 +0900

    Support OKHttp

--- webapp/java/src/main/java/net/isucon/RestClientConfig.java
+++ webapp/java/src/main/java/net/isucon/RestClientConfig.java
@@ -1,17 +1,20 @@
 package net.isucon;
 
-import java.io.IOException;
-import java.net.HttpURLConnection;
 import java.security.SecureRandom;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 
-import javax.net.ssl.*;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.X509TrustManager;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.client.OkHttpClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+
+import com.squareup.okhttp.OkHttpClient;
 
 @Configuration
 public class RestClientConfig {
@@ -40,18 +43,10 @@ public class RestClientConfig {
 
         SSLSocketFactory socketFactory = sslContext.getSocketFactory();
         HostnameVerifier verifier = (h, s) -> true;
-        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory() {
-            @Override
-            protected void prepareConnection(HttpURLConnection connection,
-                    String httpMethod) throws IOException {
-                if (connection instanceof HttpsURLConnection) {
-                    HttpsURLConnection con = (HttpsURLConnection) connection;
-                    con.setSSLSocketFactory(socketFactory);
-                    con.setHostnameVerifier(verifier);
-                }
-                super.prepareConnection(connection, httpMethod);
-            }
-        };
-        return new RestTemplate(factory);
+
+        OkHttpClient client = new OkHttpClient();
+        client.setSslSocketFactory(socketFactory);
+        client.setHostnameVerifier(verifier);
+        return new RestTemplate(new OkHttpClientHttpRequestFactory(client));
     }
 }
